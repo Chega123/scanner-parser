bdb7ee39a0c476b390c9fda0a629726c8945c3b5
Chega123 <85765351+Chega123@users.noreply.github.com>
2024-11-08 00:59:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,27 +6,28 @@ using namespace std;
 
 int main() {
 
-  string filename = "../code.txt";
-  read_file(filename, text_Arr, sizee);
+    string filename = "code.txt";
+    read_file(filename, text_Arr, sizee);
 
-  cout << endl <<  "S C A N N E R" << endl << endl;
-  scanner(text_Arr);
-  for (auto it = tokens.begin(); it != tokens.end(); ) {
-    if (it->token_name == "TOKEN_COMLINE" || 
-        it->token_name == "TOKEN_COMBLOCKS" || 
-        it->token_name == "TOKEN_COMBLOCKE") {
-        it = tokens.erase(it);  // Erase and get the next valid iterator
-    } else {
-        ++it;  // Only increment if no erase happened
+    cout << endl << "S C A N N E R" << endl << endl;
+    scanner(text_Arr);
+    for (auto it = tokens.begin(); it != tokens.end(); ) {
+        if (it->token_name == "TOKEN_COMLINE" ||
+            it->token_name == "TOKEN_COMBLOCKS" ||
+            it->token_name == "TOKEN_COMBLOCKE") {
+            it = tokens.erase(it);  // Erase and get the next valid iterator
+        }
+        else {
+            ++it;  // Only increment if no erase happened
+        }
     }
-  }
 
-  cout << endl << "T O K E N S" << endl << endl;
-  for (auto tok : tokens) {
-    cout << tok.token_name << endl;
-  }
+    cout << endl << "T O K E N S" << endl << endl;
+    for (auto tok : tokens) {
+        cout << tok.token_name << endl;
+    }
     // Crear el objeto Parser con el vector de tokens
-  
+
     cout << endl << "P A R S E R" << endl << endl;
 
     Parser parser(tokens);
@@ -39,5 +40,5 @@ int main() {
         std::cout << "Error de parsing." << std::endl;
     }
 
-  return 0;
+    return 0;
 }
